2e81af938e272bd2468aa36775f2141eedf3260e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-15 01:54:25 +0000

    drawing quads

--- poc.cpp
+++ poc.cpp
@@ -3,20 +3,61 @@
 #pragma leco add_shader "poc.vert"
 
 import casein;
+import dotz;
 import vee;
 import voo;
 import vapp;
 
+static constexpr const auto inst_count = 2;
+struct inst_t {
+  dotz::vec2 pos;
+};
+
 struct : vapp {
   void run() override {
     main_loop("yorg", [&](auto & dq, auto & sw) {
-      auto pl = vee::create_pipeline_layout();
+      voo::one_quad quad { dq.physical_device() };
+      voo::host_buffer inst {
+        dq.physical_device(),
+        vee::create_buffer(
+          inst_count * sizeof(inst_t),
+          vee::buffer_usage::vertex_buffer
+        )
+      };
 
-      voo::one_quad_render oqr { "poc", &dq, *pl };
+      {
+        voo::mapmem mm { inst.memory() };
+        auto * ptr = static_cast<inst_t *>(*mm);
+        ptr[0].pos = { 0, 0 };
+        ptr[1].pos = { 1, 0 };
+      }
+
+      auto pl = vee::create_pipeline_layout();
+      auto gp = vee::create_graphics_pipeline({
+        .pipeline_layout = *pl,
+        .render_pass = dq.render_pass(),
+        .shaders {
+          voo::shader("poc.vert.spv").pipeline_vert_stage(),
+          voo::shader("poc.frag.spv").pipeline_frag_stage(),
+        },
+        .bindings {
+          quad.vertex_input_bind(),
+          vee::vertex_input_bind_per_instance(sizeof(inst_t)),
+        },
+        .attributes {
+          quad.vertex_attribute(0),
+          vee::vertex_attribute_vec2(1, 0),
+        },
+      });;
+
       extent_loop(dq.queue(), sw, [&] {
         sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
           auto scb = sw.cmd_render_pass({ *pcb });
-          oqr.run(*scb, sw.extent());
+          vee::cmd_set_viewport(*scb, sw.extent());
+          vee::cmd_set_scissor(*scb, sw.extent());
+          vee::cmd_bind_gr_pipeline(*scb, *gp);
+          vee::cmd_bind_vertex_buffers(*scb, 1, inst.buffer());
+          quad.run(*scb, 0, 2);
         });
       });
     });
--- poc.vert
+++ poc.vert
@@ -1,10 +1,15 @@
 #version 450
 
 layout(location = 0) in vec2 pos;
+layout(location = 1) in vec2 i_pos;
 
 layout(location = 0) out vec2 f_pos;
 
 void main() {
-  gl_Position = vec4(pos * 2.0 - 1.0, 0, 1);
+  vec2 p = pos + i_pos;
+  p -= 8.0;
+  p = p / 8.0;
+
+  gl_Position = vec4(p, 0, 1);
   f_pos = pos;
 }
